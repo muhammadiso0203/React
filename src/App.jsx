afd6d9ad2a9669ab41e62fdc93bf38017b9c28d5
Muhammad <muhammadiso0203@google.com>
2025-07-23 12:03:26 +0500

    navbar full

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/header/header'
+import Hero from './components/hero/hero'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Hero/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/hero/hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Hero() {
+    return (
+        <>
+            <section>
+                <div className='text-center flex flex-col mt-28'>
+                    <h1 className='text-[56px] font-bold'>Landing template for startups</h1>
+                    <p className='text-[20px] mt-4 w-[650px] text-center mx-auto'>Our landing page template works on all devices, so you only have to
+                        set it up once, and get beautiful results forever.</p>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Hero
