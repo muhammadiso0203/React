e6779293023213a3236f0c05a3217b4eeea7c01e
Muhammad <muhammadiso0203@google.com>
2025-07-28 18:09:21 +0500

    axios life cycle fetch

--- src/components/ui/errorWidget.jsx
+++ src/components/ui/errorWidget.jsx
@@ -5,7 +5,7 @@ const ErrorWidget = ({text="Something went wrong :("}) => {
   return (
     <div className='flex flex-col items-center'>
         <img src={error} alt="" />
-        <p className='text-red-500 text-center'>{text}</p>
+        <p className='text-red-600 text-center'>{text}</p>
     </div>
   )
 }
